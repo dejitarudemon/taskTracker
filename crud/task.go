490b099d6feaef9808c433e4824f4c805c833308
dejitarudemon <fine.usr1@yandex.ru>
2024-10-11 15:12:24 +0300

    added task struct

--- crud/task.go
+++ crud/task.go
@@ -3,7 +3,14 @@ The package for CRUD-operations for Task Tracker App
 */
 package crud
 
+import "time"
+
 type Task struct {
+	id          int
+	description int
+	status      Status
+	createdAt   time.Time
+	updatedAt   time.Time
 }
 
 type Status []byte
