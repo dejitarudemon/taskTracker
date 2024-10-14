395c9912d2e76691a5e91c2d651e42c9ae760643
dejitarudemon <fine.usr1@yandex.ru>
2024-10-14 12:10:50 +0300

    added Add function

--- crud/operations.go
+++ crud/operations.go
@@ -3,16 +3,56 @@ The package for CRUD-operations for Task Tracker App
 */
 package crud
 
-import "os"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"os"
+)
 
 const FILEDATA = "./tasks.json"
 
 func (task *Task) Add(descriptrion []byte) error {
 	file, err := os.Open(FILEDATA)
 	if err != nil {
-		return &TaskError{id: 0, msg: "No data file"}
+		return err
 	}
 	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	if !json.Valid(content) {
+		return errors.New("Not Valid Structure")
+	}
+
+	var tasks []Task
+
+	if err := json.Unmarshal(content, &tasks); err != nil {
+		return err
+	}
+
+	var last_id int
+	if len(tasks) > 0 {
+		last_id = tasks[len(tasks)-1].id
+	} else {
+		last_id = 0
+	}
+
+	task.id = last_id + 1
+	tasks = append(tasks, *task)
+
+	tasksJsoned, err := json.Marshal(tasks)
+	if err != nil {
+		return err
+	}
+
+	if _, err := file.Write(tasksJsoned); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (task *Task) Update(new_descriptrion []byte) error {}
